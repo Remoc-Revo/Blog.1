dcc4c651b1245dba40fbad909e834ad6dee869b6
Remoc-Revo <timothynguono@gmail.com>
2023-09-10 17:22:04 +0300

    Update changes for remote hosting

--- client/src/components/article_preview.js
+++ client/src/components/article_preview.js
@@ -5,7 +5,7 @@ export  function PreviewBig({headline,time,briefDescription,imgUrl,articleId}){
         console.log("urlllll",imgUrl)
         return(
             <a href={`/sngl/${articleId}`} style={{textDecoration:"none",color:"black"}} className="">
-                <img src={require(`../../public/uploads/${imgUrl}`)} alt=""   style={{display:"block",width:"100%",height:"500px"}}></img>
+                <img src={require(`${imgUrl}`)} alt=""   style={{display:"block",width:"100%",height:"500px"}}></img>
 
                 <div  className="container">
                     <i className="duration">{time}</i>
@@ -22,7 +22,7 @@ export  function PreviewMid({headline,time,briefDescription,imgUrl,articleId}){
     
     return(
         <a href={`/sngl/${articleId}`} style={{textDecoration:"none",padding:"0px"}} className="col preview-mid">
-            <img src={require(`../../public/uploads/${imgUrl}`)} alt="" className="" style={{display:"block",width:"100%",height:"300px"}}></img>
+            <img src={require(`${imgUrl}`)} alt="" className="" style={{display:"block",width:"100%",height:"300px"}}></img>
 
             <div style={{color:"black"}} className="container">
                 <i className="duration">{time}</i>
@@ -38,7 +38,7 @@ export  function PreviewSmall({headline,time,briefDescription,imgUrl,articleId})
     
     return(
         <a href={`/sngl/${articleId}`}  style={{textDecoration:"none",color:"black"}} className="mb-4 mt-3 d-flex container preview-small" >
-            <img src={require(`../../public/uploads/${imgUrl}`)} alt="" className="col-md-5" style={{display:"block",width:"200px",height:"100px"}}></img>
+            <img src={require(`${imgUrl}`)} alt="" className="col-md-5" style={{display:"block",width:"200px",height:"100px"}}></img>
             
             <div style={{}} className="ms-4">
                 <i className="duration">{time}</i>
