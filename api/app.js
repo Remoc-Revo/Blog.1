4f8034816b3223096121140a46dd4ad105505bd1
Remoc-Revo <69009252+Remoc-Revo@users.noreply.github.com>
2023-09-13 19:43:23 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -4,10 +4,10 @@ const sls = require('serverless-http');
 const app = express();
 require('dotenv').config();
 
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// const cors=require('cors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors=require('cors');
 
 const session=require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
@@ -15,32 +15,33 @@ const createPool=require('./config/dbConnection');
 const pool = createPool();
 const sessionStore=new MySQLStore({},pool);
 
-// const  indexRouter = require('./routes/index');
 
-// // app.set('view engine', 'jade');
+const  indexRouter = require('./routes/index');
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+// app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-// app.use(cors({
-//   allowedHeaders:['Content-Type'],
-//   credentials:true,
-//   origin: 'http://localhost:3000'
-// }));
+app.use(cors({
+  allowedHeaders:['Content-Type'],
+  credentials:true,
+  origin: 'http://localhost:3000'
+}));
 
-// app.use(session({
-//   secret:'secreet',
-//   saveUninitialized:true,
-//   resave:false,
-//   store: sessionStore
-// }))
+app.use(session({
+  secret:'secreet',
+  saveUninitialized:true,
+  resave:false,
+  store: sessionStore
+}))
 
-// app.use(indexRouter);
+app.use(indexRouter);
 
 app.get('/ye',async (req,res,next)=>{res.status(200).send("Yeaaaaa"+process.env.DB_NAME)})
 
