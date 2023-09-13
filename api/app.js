ab573cbdc7f3aa072563de039392f031871104b8
Remoc-Revo <69009252+Remoc-Revo@users.noreply.github.com>
2023-09-13 20:07:15 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -34,12 +34,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 //   origin: 'http://localhost:3000'
 // }));
 
-// app.use(session({
-//   secret:'secreet',
-//   saveUninitialized:true,
-//   resave:false,
-//   store: sessionStore
-// }))
+app.use(session({
+  secret:'secreet',
+  saveUninitialized:true,
+  resave:false,
+  store: sessionStore
+}))
 
 app.use(indexRouter);
 
