cef713650f5e3e20ffae3572ce59cbf24dca3a42
Remoc-Revo <69009252+Remoc-Revo@users.noreply.github.com>
2023-09-13 20:04:08 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -20,11 +20,11 @@ const  indexRouter = require('./routes/index');
 
 // app.set('view engine', 'jade');
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
