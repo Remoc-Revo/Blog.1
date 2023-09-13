2f95393e84bd7103dca0399b7e9bda14b6982b64
Remoc-Revo <69009252+Remoc-Revo@users.noreply.github.com>
2023-09-13 19:30:15 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -2,47 +2,47 @@ var createError = require('http-errors');
 var express = require('express');
 const sls = require('serverless-http');
 const app = express();
-
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors=require('cors');
-const session=require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
-const createPool=require('./config/dbConnection');
-const pool = createPool();
 require('dotenv').config();
 
-const sessionStore=new MySQLStore({},pool);
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// const cors=require('cors');
+// const session=require('express-session');
+// const MySQLStore = require('express-mysql-session')(session);
+// const createPool=require('./config/dbConnection');
+// const pool = createPool();
 
-const  indexRouter = require('./routes/index');
+// const sessionStore=new MySQLStore({},pool);
 
-// app.set('view engine', 'jade');
+// const  indexRouter = require('./routes/index');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// // app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.use(cors({
-  allowedHeaders:['Content-Type'],
-  credentials:true,
-  origin: 'http://localhost:3000'
-}));
+// app.use(cors({
+//   allowedHeaders:['Content-Type'],
+//   credentials:true,
+//   origin: 'http://localhost:3000'
+// }));
 
-app.use(session({
-  secret:'secreet',
-  saveUninitialized:true,
-  resave:false,
-  store: sessionStore
-}))
+// app.use(session({
+//   secret:'secreet',
+//   saveUninitialized:true,
+//   resave:false,
+//   store: sessionStore
+// }))
 
-app.use(indexRouter);
+// app.use(indexRouter);
 
-app.get('/',async (req,res,next)=>{res.status(200).send("Yeaaaaa")})
+app.get('/ye',async (req,res,next)=>{res.status(200).send("Yeaaaaa"+process.env.DB_NAME)})
 
 
 module.exports.server = sls(app);
