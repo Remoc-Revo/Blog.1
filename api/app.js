94d3cbf7bfe7acbdab3a23f3ad7e7d805e147217
Remoc-Revo <timothynguono@gmail.com>
2023-09-17 08:16:46 +0300

    Update responses to allow CORS

--- api/app.js
+++ api/app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(cors({
-  allowedHeaders:['Content-Type','Access-Control-Allow-Origin'],
+  allowedHeaders:['Content-Type'],
   credentials:true,
   origin: process.env.CLIENT_HOST
 }));
