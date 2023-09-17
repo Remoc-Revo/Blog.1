380398b0274f286b0e494d691b5b1a310c18aa06
Remoc-Revo <timothynguono@gmail.com>
2023-09-17 20:00:06 +0300

    fix cors

--- client/src/pages/articleUpdating.js
+++ client/src/pages/articleUpdating.js
@@ -20,6 +20,7 @@ export default function ArticlesUpdating(){
     useEffect(()=>{
         if(!loading && user != null){
             if(user === 'unauthorized'){
+                console.log("the user's context ",user)
                 navigate('/login')
             }
         }
