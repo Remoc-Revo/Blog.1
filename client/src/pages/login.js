906949351e175315da572353e510543a9dce3c2e
Remoc-Revo <69009252+Remoc-Revo@users.noreply.github.com>
2023-09-17 16:17:53 +0300

    Update login.js

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -9,12 +9,13 @@ export default function Login(){
     var [email,set_email]=useState();
     var [password,set_password]=useState();
     var [errorMessage,set_errorMessage]=useState();
-    const {user,contextLogin} = useUserContext();
+    const {user,contextLogin,loggingIn} = useUserContext();
     
     function login(e){
         e.preventDefault();
         set_errorMessage();
-
+        loggingIn();
+        
         api.post('/login',
             {
                 withCredentials:true,
