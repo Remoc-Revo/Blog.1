e344d81fd6991d0f115cdfaa8f0703c2f8929757
Remoc-Revo <69009252+Remoc-Revo@users.noreply.github.com>
2023-09-17 16:54:11 +0300

    Update login.js

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -9,8 +9,7 @@ export default function Login(){
     var [email,set_email]=useState();
     var [password,set_password]=useState();
     var [errorMessage,set_errorMessage]=useState();
-    const {user,contextLogin,loggingIn} = useUserContext();
-    loggingIn();
+    const {user,contextLogin} = useUserContext();
 
     function login(e){
         e.preventDefault();
@@ -24,7 +23,7 @@ export default function Login(){
             })
             .then((response)=>{
                 if(response && response.status===200){
-                    contextLogin(response.data)
+                    contextLogin();
                     console.log("user after login ",user);
                     navigate("/");
                 }
