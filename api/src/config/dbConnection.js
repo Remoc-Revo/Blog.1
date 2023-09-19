1d4d9106d00fc50735e1eb4d900094dacae9ff1b
Remoc-Revo <69009252+Remoc-Revo@users.noreply.github.com>
2023-09-19 18:30:44 +0300

    Update dbConnection.js

--- api/src/config/dbConnection.js
+++ api/src/config/dbConnection.js
@@ -10,6 +10,7 @@ const createPool=()=>{
                     user:process.env.DB_USER,
                     password:process.env.DB_PASSWORD,
                     database:process.env.DB_NAME,
+                    port: process.env.DB_PORT,
                     waitForConnections:true,
                     connectionLimit:15,
                     queueLimit:0
