4b337111071ebc23b11f2b991fe91ef08196148b
Remoc-Revo <timothynguono@gmail.com>
2023-09-21 09:48:39 +0300

    Fixing session

--- api/src/app.js
+++ api/src/app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   secret:'secreet',
-  saveUninitialized:true,
+  saveUninitialized:false,
   resave:false,
   store: sessionStore,
   cookie: {
