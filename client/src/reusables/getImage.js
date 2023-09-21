afb70fb97d1df5a11c01e19c1ecc1ed4f863f18b
Remoc-Revo <timothynguono@gmail.com>
2023-09-21 11:43:47 +0300

    fix image display

--- client/src/reusables/getImage.js
+++ client/src/reusables/getImage.js
@@ -1,17 +1,19 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 export default function fetchImage(imgUrl){
     return new Promise((resolve,reject)=>{
     
-        axios
-            .get(imgUrl)
-            .then((response)=>{
-                resolve(response.config.url);
-            })
-            .catch((err)=>{
-                console.log('Error fetching image from s3',err);
-                reject(err);
-            })
+        // axios
+        //     .get(imgUrl,{withCredentials:true})
+        //     .then((response)=>{
+        //         resolve(response.config.url);
+        //     })
+        //     .catch((err)=>{
+        //         console.log('Error fetching image from s3',err);
+        //         reject(err);
+        //     })
+
+        resolve(imgUrl);
     })
     
 }
