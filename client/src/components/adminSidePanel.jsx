b422e7763f2cf42fc6ec576e2e82d0d06d968a28
Remoc-Revo <timothynguono@gmail.com>
2024-07-05 23:45:30 +0300

    Update admin side panel's layout

--- client/src/components/adminSidePanel.jsx
+++ client/src/components/adminSidePanel.jsx
@@ -1,12 +1,17 @@
 
-
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { faHouseChimney } from "@fortawesome/free-solid-svg-icons";
 import Logo from "../logos/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChartSimple } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { faComment } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { useLocation } from "react-router-dom";
 
-export default function AdminSidePanel(){
-
+ const AdminSidePanel=()=>{
+    let adminPanel = ""//useLocation().search;
+    // document.write("adminPanel ",adminPanel);
 
     return <div className="text-white">
 
@@ -22,21 +27,49 @@ export default function AdminSidePanel(){
                 </div>
             </div>
            
-            <div className="mt-4">
-                <div className="d-flex gap-2 ps-3">
-                    <FontAwesomeIcon icon={faHome}/>
-                    <p className="text-light">My Home</p>
-                </div>
-                <div className="d-flex gap-2 ps-3">
+            <div className="mt-4 d-flex flex-column gap-2 admin-side-panel-links">
+                <a href="/" 
+                   className="d-flex gap-2 ps-3 pt-1 pb-1"
+                   id={adminPanel===""?"active":""}
+                   >
+                    <FontAwesomeIcon icon={faHouseChimney} className=""/>
+                    <p className=" m-0">My Home</p>
+                </a>
+                <a  href="/?adminPanel=stats"
+                    className="d-flex gap-2 ps-3 pt-1 pb-1 align-items-center"
+                    id={adminPanel==="?adminPanel=stats"?"active":""}
+                     >
                     <FontAwesomeIcon icon={faChartSimple}/>
-                    <p className="text-light">Stats</p>
-                </div>
+                    <p className="pt-1 m-0">Stats</p>
+                </a>
 
-                <div className="d-flex gap-2 ps-3">
-                    <FontAwesomeIcon icon={faHome}/>
-                    <p className="text-light">Posts</p>
-                </div>
+                <a href="/?adminPanel=posts" 
+                   className="d-flex gap-2 ps-3 pt-1 pb-1"
+                   id={adminPanel==="?adminPanel=posts"?"active":""}
+                   >
+                    <FontAwesomeIcon icon={faCalendar}/>
+                    <p className=" m-0">Posts</p>
+                </a>
+
+                <a href="/?adminPanel=comments" 
+                   className="d-flex gap-2 ps-3 pt-1 pb-1"
+                   id={adminPanel==="?adminPanel=comments"?"active":""}
+                   >
+                    <FontAwesomeIcon icon={faComment} className=""/>
+                    <p className=" m-0">Comments</p>
+                </a>
+
+                <a href="/?adminPanel=users" 
+                   className="d-flex gap-2 ps-3 pt-1 pb-1"
+                   id={adminPanel==="?adminPanel=users"?"active":""}
+                   >
+                    <FontAwesomeIcon icon={faUser} className=""/>
+                    <p className=" m-0">Users</p>
+                </a>
             </div>
         </div>
     </div>
 }
+
+
+export default React.memo(AdminSidePanel);
