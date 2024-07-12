0a69032cfb0f19f9b6db3aae2ffeea13065cc219
Remoc-Revo <timothynguono@gmail.com>
2024-07-12 22:44:55 +0300

    Add: Logging of views and visitors of single articles pages

--- client/src/reusables/global.js
+++ client/src/reusables/global.js
@@ -1,5 +1,6 @@
 import parser from "html-react-parser";
-
+import {v4 as uuidv4} from 'uuid'
+import api from "../config/api";
 
 export function decodeString(str){
     return parser(decodeURIComponent(str)
@@ -16,3 +17,24 @@ export function updateHistory(path){
         window.history.pushState({},'',path)
     }
 }
+
+
+const getVisitorId = ()=>{
+    let visitorId = localStorage.getItem("visitorId");
+    if(!visitorId){
+        visitorId = uuidv4();
+        localStorage.setItem("visitorId",visitorId);
+    }
+
+    return visitorId;
+};
+
+export async function logVisitor(path){
+    const visitorId = getVisitorId();
+    try{
+        api.post('/visitor',{path,visitorId});
+    }
+    catch(e){
+        console.log("Error logging visitor");
+    }
+};
