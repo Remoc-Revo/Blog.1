35ce1ee4935cf0d7cd6290b02a0dc007d2e0c98b
Remoc-Revo <timothynguono@gmail.com>
2024-07-14 12:40:55 +0300

    Add: Indication status and visibility of article being edited

--- api/src/routes/addArticle.js
+++ api/src/routes/addArticle.js
@@ -37,7 +37,7 @@ exports.addArticle=(req,res)=>{
                         if(result){
                         }
                     })
-                    return res.status(200).json({})
+                return res.status(200).json({articleId})
 
              }) 
     }
