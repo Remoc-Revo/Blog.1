8bee25771b5181fb6dfcd8fbee251195743854ff
Remoc-Revo <timothynguono@gmail.com>
2024-07-16 19:09:57 +0300

    Add: Related section in single article view

--- api/src/routes/getArticles.js
+++ api/src/routes/getArticles.js
@@ -58,3 +58,42 @@ exports.single= async (req,res)=>{
     console.log("resullllt",article,"likes", likes)
     return res.status(200).json({article:article,likes:likes})
 }
+
+
+exports.related=(req,res)=>{
+    let {lastArticleId} = req.query;
+    lastArticleId = parseInt(lastArticleId,10)
+    console.log("the cat",req.query.cat)
+
+    const fetchQuery = `
+        SELECT * 
+        FROM ARTICLE  
+        JOIN SECTION ON ARTICLE.articleSectionId = SECTION.sectionId
+        JOIN MULTIMEDIA ON ARTICLE.articleId=MULTIMEDIA.articleId 
+
+        WHERE ( 
+        CASE 
+            WHEN ? !=0 THEN ARTICLE.articleId < ?
+            ELSE TRUE
+        END
+        )
+        ${req.query.cat ? 'AND SECTION.sectionName = ?' : ''}
+        AND articleIsDraft = 0
+        ORDER BY articlePostingDate DESC 
+        LIMIT 4
+    `;
+
+    
+
+    pool.query(fetchQuery,[lastArticleId,lastArticleId,req.query.cat],
+        (err,result)=>{
+            if(err){
+                console.log(err);
+            }
+            const articles=result;
+            console.log("the last  article",lastArticleId)
+            console.log("articles: ",result);
+            res.status(200).json({articles}); 
+        }) 
+
+}
--- client/src/components/more.jsx
+++ client/src/components/more.jsx
@@ -1,12 +1,12 @@
 import React,{useState,useEffect} from "react";
 import api from "../config/api";
-import GetImage from "../reusables/getImage";
+import getFirstImage from "../reusables/getImage";
 
-export default function More({cat,current}){
+export default function Related({cat,current}){
    var [more,set_more]=useState([]);
 
    useEffect(()=>{
-      api.get(`/?cat=${cat}`,
+      api.get(`/related?cat=${cat}`,
          {withCredentials:true,
           params:{
             lastArticleId:0
@@ -22,17 +22,22 @@ export default function More({cat,current}){
    },[cat])
 
    return(
-      <div className="col-lg-3 mt-6 border ">
-         <div className="row container ">
-            <h5>More like this</h5>
+      <div className=" mt-5 "
+            style={{borderTop:"1px solid lightgrey"}}
+         >
+         <div className="row container pt-2 ">
+            <h5>Related</h5>
          </div>
 
-         <div className="container-lg" >
+         <div className="d-md-flex " >
            {
             more.map((article,index)=>{
                if(index <= 5 && article.articleId !== current){
-                     return <SinglePreview headline={article.articleHeadline}  
-                      imgUrl={article.multimediaUrl} articleId={article.articleId}/>                
+                     return <RelatedPreview headline={article.articleHeadline}  
+                        imgUrl={getFirstImage(article.articleBody)} 
+                        articleId={article.articleId}
+                        datePosted = {article.articlePostingDate}
+                        />                
                                                
                }
                else{
@@ -50,33 +55,28 @@ export default function More({cat,current}){
 }
 
 
-function SinglePreview({headline,imgUrl,articleId}){
-   const [fetchedImgUrl,setFetchedImgUrl] = useState('');
+function RelatedPreview({headline,imgUrl,articleId,datePosted}){
+   const date = new Date(datePosted);
+   const dateStr =   new Intl.DateTimeFormat('en-US',{month:'long',day:'numeric',year:'numeric'}).format(date);
 
-   useEffect(()=>{
-      async function fetchImage(){
-            try{
-               const url = await GetImage(imgUrl);
-               setFetchedImgUrl(url)
-               console.log("urlllll",url)
+   return <a href={`/sngl/${articleId}`} 
+            className="col-md-4 col-sm-12 d-flex flex-md-column text-decoration-none row-xs mb-3 gap-2 p-2">
+            <div className="container-xs col-5 col-md-12"
+               style={{height:"150px"}}
+               >
+                  <img src={imgUrl} alt="" className="object-fit-cover rounded" 
+                        style={{height:"100%",width:"100%"}}
+                  />
 
-            }catch(err){
-               console.log('error fetching image',err);
-            }
-      }
+            </div>
 
-      fetchImage();
-   },[imgUrl])
-
-   return <a href={`/sngl/${articleId}`} className=" d-flex d-lg-block text-decoration-none row-xs mb-3 gap-3">
-                              <div className="col-4 col-sm-4  col-lg-12 container-xs">
-                                 <img src={fetchedImgUrl} alt="" className=" w-100 " 
-                                       style={{maxHeight:"150px"}}
-                                 />
-
-                              </div>
-                                 
-                              <p className="" style={{fontSize:"15px",color:"teal"}}>{decodeURIComponent(headline).replace(/&apos;/g,"'")}</p>
+             <div>
+               <h6 className="fw-lighter" style={{fontSize:"16px",color:"grey"}}>{dateStr}</h6>
+               <h6 className="fw-lighter" style={{fontSize:"16px",color:"black"}}>
+                  {decodeURIComponent(headline).replace(/&apos;/g,"'")}
+               </h6>
+            </div>  
+            
 
          </a>
 
