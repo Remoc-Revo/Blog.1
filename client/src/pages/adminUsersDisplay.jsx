143334446d8a69c218f53245a8e5c3dc8b25758d
Remoc-Revo <timothynguono@gmail.com>
2024-07-19 10:38:39 +0300

    Update admin user layout

--- client/src/pages/adminUsersDisplay.jsx
+++ client/src/pages/adminUsersDisplay.jsx
@@ -1,18 +1,22 @@
 
 import React,{useState,useEffect} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser,faSearch,faTimes } from "@fortawesome/free-solid-svg-icons";
 import api from "../config/api";
-
+import moment from "moment";
 
 export default function AdminUsersDisplay(){
-    const [users, setUsers] = useState([]);
+    const [allUsers, setAllUsers] = useState([]);
+    const [displayedUsers, setDisplayedUsers] = useState([])
+    const [searchedText, setSearchedText] = useState('');
+    const [isSearchInputActive, setIsSearchInputActive] = useState(false);
 
     useEffect(()=>{
         api.get('/users',)
             .then((response)=>{
                 console.log("fetched users",response.data.users);
-                setUsers(response.data.users)
+                setAllUsers(response.data.users)
+                setDisplayedUsers(response.data.users)
             })
             .catch((e)=>{
                 console.log("Error fetching users: ",e);
@@ -20,29 +24,100 @@ export default function AdminUsersDisplay(){
     },[])
 
 
+    function handleSearch(e){
+        const text = e.target.value.toLowerCase();
+        setSearchedText(text);     
+
+        const filteredUsers = allUsers.filter(
+            user => 
+                {
+                    const userName = user.userName.toLowerCase();
+                    const userEmail = user.userEmail.toLowerCase();
+                    
+                    if(userName.includes(text) || userEmail.includes(text)){            
+                        return user;
+                    }
+
+                    return null;
+                 }
+        )
+
+        setDisplayedUsers(filteredUsers);
+    }
+
+
 
     return <div className="container col-lg-7 col-md-8">
 
-        <h3 className="mb-5">Users</h3>
-        <div>
+        <h3 className="mb-5">All Users {allUsers.length !== 0 && `(${allUsers.length})`}</h3>
+        <div className="d-flex  mb-4 " id="users-search">
+            <div className="col-12 position-relative d-flex justify-content-between border" >                
+                <button className="btn rounded-0">
+                    <FontAwesomeIcon icon={faSearch}/>
+                </button>                
+                
+                <div className="">
+                    <button className="btn position-absolute start-0 top-0 h-100"
+                        // onClick={}
+                        style={{zIndex:"2",color:"grey"}}>
+                        <FontAwesomeIcon icon={faSearch}/>
+                    </button>
+                    <input 
+                        type="text"
+                        className="position-absolute w-100  ps-5 pe-5 start-0 top-0 h-100 border-0"
+                        placeholder=" Search user name or email..."
+                        onClick={()=>{setIsSearchInputActive(true)}}
+                        onChange={handleSearch}
+                        value={searchedText}
+                        style={{zIndex:"1"}}
+                        />
+                    
+                { isSearchInputActive 
+                    &&<button className="btn position-absolute end-0 top-0 h-100"
+                        onClick={()=>{
+                            setIsSearchInputActive(false);
+                            setSearchedText('')
+                            setDisplayedUsers(allUsers);
+                        }}
+                        style={{zIndex:"1"}}>
+                        <FontAwesomeIcon icon={faTimes} className="ic-grey"/>
+                    </button>}
+                </div>
+                    
+                
+            </div>
+                
+            </div>
+
+            <div>
             <table className="w-100 " >
                 {
-                    users.map(
+                    displayedUsers.map(
                         user=><tr className="" >
-                            <div className=" d-flex gap-4 ms-4 me-4 align-items-center" style={{height:"90px"}}>
-                                <div className="rounded-circle "
-                                    style={{width:"70px",height:"70px",backgroundColor:"lightgrey"}}
-                                    >
-                                    <FontAwesomeIcon className="ic-white pt-2 " icon={faUser}  
-                                        style={{width:"100%",height:"100%"}}/>
+                            <div className=" d-flex gap-4 ms-4 me-4 pt-2 align-items-start" style={{height:"100px"}}>
+                            {
+                                    (user.photoUrl !== null)
+                                    ?<div style={{width:"70px",height:"70px"}}>                                                                                           
+                                        <img src={user.photoUrl}
+                                            className="rounded-circle w-100 h-100 object-fit-cover"
+                                            alt=""
+                                        />
+                                    </div>
 
-                                </div>
+                                    :<div className="rounded-circle overflow-hidden" 
+                                        style={{width:"70px",height:"70px", backgroundColor:"lightgrey"}}>
+                                        <FontAwesomeIcon icon={faUser} className="ic-white rounded-circle w-100 h-100 pt-2"/>
+                                    </div>
+                                }
                                 <div>
-                                    <span className="fw-bolder">{user.userName}</span>
+                                    <span className="" style={{fontWeight:"500"}}>{user.userName}</span>
+                                    <span className="d-block" style={{fontWeight:"300"}}>{user.userEmail}</span>
+
                                     {
                                         user.userLevel === 0 &&
-                                        (<div>
-                                            <span style={{color:"teal"}}>Reader</span>
+                                        (<div className="mt-1">
+                                            <span style={{color:"teal"}}>Registered</span>
+                                            <span className="fw-light"> {moment(user.userRegistrationDate).fromNow()}</span>
                                         </div>)                                        
                                     }
                                     {     user.userLevel === 1 &&
