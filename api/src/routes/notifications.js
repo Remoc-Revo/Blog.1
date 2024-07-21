838144279bfd929e149e66f3347810093673bdef
Remoc-Revo <timothynguono@gmail.com>
2024-07-21 22:53:17 +0300

    Update comment layout

--- api/src/routes/notifications.js
+++ api/src/routes/notifications.js
@@ -1,10 +1,20 @@
-
-const createPool=require('../config/dbConnection')
-const pool = createPool();
+const {queryDb} = require('../global')
 
 
 
-exports.getAdminNotifications = (req,res)=>{
 
-    pool.query(``)
+exports.getAdminNotifications = async(req,res)=>{
+
+   const comments = await queryDb(`SELECT c.comment,
+                       c.dateAdded,
+                       u.userName,
+                       p.photoUrl as commenterProfilePhoto,
+                       a.articleHeadline
+                FROM COMMENT c
+                JOIN USER u ON u.userId = c.userId
+                JOIN ARTICLE a on a.articleId = c.articleId
+                LEFT JOIN USERPHOTO p ON p.userId = c.userId
+                WHERE c.parentCommentId is NULL 
+                ORDER BY c.dateAdded DESC
+                `)
 }
