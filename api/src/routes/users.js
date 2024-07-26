d02535073cc8a03106f8402850262ca9225237b0
Remoc-Revo <timothynguono@gmail.com>
2024-07-26 18:34:47 +0300

    Update email sending configurations

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -304,7 +304,7 @@ exports.requestPasswordReset = async(req,res)=>{
   
       const mailOptions = {
         to: email,
-        from: process.env.EMAIL_USER,
+        from: process.env.PSWD_EMAIL_USER,
         subject: 'Password Reset',
         text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
                Please click on the following link, or paste this into your browser to complete the process:\n\n
