d7853345cdb18925b7cb42a6b258ab4f6514e3aa
Remoc-Revo <timothynguono@gmail.com>
2024-07-29 08:39:42 +0300

    Update api middleware

--- api/src/app.js
+++ api/src/app.js
@@ -24,8 +24,8 @@ const  indexRouter = require('./routes/index');
 app.set('trust proxy', 1);
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
