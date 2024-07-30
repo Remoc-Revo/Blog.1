383a5bb65059e9b757bc96137c5a4bf0d0d4ce0a
Remoc-Revo <timothynguono@gmail.com>
2024-07-30 17:21:56 +0300

    update footer

--- client/server/server.js
+++ client/server/server.js
@@ -8,27 +8,6 @@ const  App = require("../src/App").default;
 
 const app = express();
 
-// app.get("/*", (req, res) => {
-//   const entryPoint = ["/main.js"];
-
-//   const { pipe, abort: _abort } = ReactDOMServer.renderToPipeableStream(
-//     <StaticRouter location={req.url}>
-//       <App />
-//     </StaticRouter>,
-//     {
-//       bootstrapScripts: entryPoint,
-//       onShellReady() {
-//         res.statusCode = 200;
-//         res.setHeader("Content-type", "text/html");
-//         pipe(res);
-//       },
-//       onShellError() {
-//         res.statusCode = 500;
-//         res.send("<!doctype html><p>Loading...</p>");
-//       },
-//     }
-//   );
-// });
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
