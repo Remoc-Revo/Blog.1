d81c3676ee5a3a41706fa4976f451083e309f099
Remoc-Revo <timothynguono@gmail.com>
2024-07-31 12:16:41 +0300

    Seperate client server from main client codnt code

---
+++ client_server/server.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+import express from "express";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import { StaticRouter } from "react-router-dom/server";
+const  App = require("../client/src/App").default;
+
+const app = express();
+
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/*', (req, res) => {
+  const context = {};
+  const appString = ReactDOMServer.renderToString(
+      <StaticRouter location={req.url} context={context}>
+        <App />
+      </StaticRouter>
+  );
+
+  const indexFile = path.resolve(__dirname, '../client/build/index.html');
+  fs.readFile(indexFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, Server error!');
+    }
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${appString}</div>`)
+    );
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log("App is running...");
+});
