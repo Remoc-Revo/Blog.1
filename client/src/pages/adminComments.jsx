52e4fefc5be0b533a9e3a0008692cde78c3719ef
Remoc-Revo <timothynguono@gmail.com>
2024-07-31 17:47:50 +0300

    Revert "Add space to every new line"
    This reverts commit f458e0567fd8cb163212e451843f092b2f704964.

--- client/src/pages/adminComments.jsx
+++ client/src/pages/adminComments.jsx
@@ -1,99 +1,99 @@
 
- import React,{useState,useEffect} from "react";
- import api from "../config/api";
- import { decodeString,formatDateTime } from "../reusables/global";
- import { useNavigate } from "react-router-dom";
- import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
- import { faUser } from "@fortawesome/free-solid-svg-icons";
- import SessionEndedModal from "../reusables/sessionEndedModal";
- 
- export default function AdminComments(){
-     const [comments, setComments] = useState([]);
-     const [isLoading, setIsLoading] = useState(false);
-     const navigate = useNavigate();
-     const [showSessionEndedModal, setShowSessionEndedModal] = useState(false);
- 
-     useEffect(()=>{
-          function fetchAdminComments(){
-             setIsLoading(true);
-             api.get("/adminComments",{withCredentials:true})
-                .then((response)=>{
-                     console.log("response",response.data.comments)
-                     
-                     setComments(response.data.comments);
-                     setIsLoading(false);
-                })
-                .catch((e)=>{
-                 setIsLoading(false);
-                 console.log("Error fetching admin data", e);
-                 if(e.response.status === 401){
-                     navigate('/');
-                     setShowSessionEndedModal(true);
-                 }
-                })
-         }
-         fetchAdminComments();
-     },[])
- 
-     return <div className="container col-lg-10 ">
- 
-         <div className="container mb-5">
-             <h2>Comments</h2>
-             {
-                 isLoading &&
-                 <div className="d-flex justify-content-center">
-                     <div className="spinner-border text-success ">
-                         <span className="sr-only">Loading...</span>
-                     </div>
-                 </div>
-             }
-             {
-                 (comments.length===0 && !isLoading)
-                 ?<div className="d-flex justify-content-center"><h5 className="fw-lighter">No Comment Yet</h5></div>
-                 :<div className="mt-5">
-                     {
-                         comments.map((comment)=>{
-                             return <div className={`pt-2 d-flex gap-3 border-bottom`}
-                                     onClick={()=>navigate(`/sngl/${comment.articleId}?scrollTo=comment${comment.commentId}`)}
-                                 >
-                             <div>
-                                 {
-                                     (comment.commenterProfilePhoto !== null)
-                                     ?<div style={{width:"50px",height:"50px"}}>                                                                                           
-                                         <img src={comment.commenterProfilePhoto}
-                                             className="rounded-circle w-100 h-100 object-fit-cover"
-                                             alt=""
-                                         />
-                                     </div>
- 
-                                     :<div className="rounded-circle overflow-hidden" 
-                                         style={{width:"50px",height:"50px", backgroundColor:"grey"}}>
-                                         <FontAwesomeIcon icon={faUser} className="ic-light-grey rounded-circle w-100 h-100 pt-2"/>
-                                     </div>
-                                 }
-                             </div>
-                             <div className=" d-flex flex-column gap-1">
-                                 <span style={{fontWeight:"500"}}>{comment.userName.toUpperCase()}</span>
-                 
-                                 <span className="" style={{color:"grey",fontSize:"13px"}}>{formatDateTime(comment.dateAdded)}</span>
-                 
-                                 <p className="mt-3" style={{fontWeight:"300"}}>{comment.comment}</p>
- 
-                                 <div className=" d-flex gap-3 align-items-start">
-                                     <h6 className="fw-lighter">On: </h6>
-                                     <h6 className="" >{decodeString(comment.articleHeadline)}</h6>
-                                 </div>
-                                
-                             </div>
-                             
-                         </div>
-                         })
-                     }
-                 </div>
-             }
-        </div>
-                <SessionEndedModal showSessionEndedModal={showSessionEndedModal} setShowSessionEndedModal={setShowSessionEndedModal}/>
- 
- 
-     </div> 
- }
+import React,{useState,useEffect} from "react";
+import api from "../config/api";
+import { decodeString,formatDateTime } from "../reusables/global";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import SessionEndedModal from "../reusables/sessionEndedModal";
+
+export default function AdminComments(){
+    const [comments, setComments] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+    const [showSessionEndedModal, setShowSessionEndedModal] = useState(false);
+
+    useEffect(()=>{
+         function fetchAdminComments(){
+            setIsLoading(true);
+            api.get("/adminComments",{withCredentials:true})
+               .then((response)=>{
+                    console.log("response",response.data.comments)
+                    
+                    setComments(response.data.comments);
+                    setIsLoading(false);
+               })
+               .catch((e)=>{
+                setIsLoading(false);
+                console.log("Error fetching admin data", e);
+                if(e.response.status === 401){
+                    navigate('/');
+                    setShowSessionEndedModal(true);
+                }
+               })
+        }
+        fetchAdminComments();
+    },[])
+
+    return <div className="container col-lg-10 ">
+
+        <div className="container mb-5">
+            <h2>Comments</h2>
+            {
+                isLoading &&
+                <div className="d-flex justify-content-center">
+                    <div className="spinner-border text-success ">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
+            }
+            {
+                (comments.length===0 && !isLoading)
+                ?<div className="d-flex justify-content-center"><h5 className="fw-lighter">No Comment Yet</h5></div>
+                :<div className="mt-5">
+                    {
+                        comments.map((comment)=>{
+                            return <div className={`pt-2 d-flex gap-3 border-bottom`}
+                                    onClick={()=>navigate(`/sngl/${comment.articleId}?scrollTo=comment${comment.commentId}`)}
+                                >
+                            <div>
+                                {
+                                    (comment.commenterProfilePhoto !== null)
+                                    ?<div style={{width:"50px",height:"50px"}}>                                                                                           
+                                        <img src={comment.commenterProfilePhoto}
+                                            className="rounded-circle w-100 h-100 object-fit-cover"
+                                            alt=""
+                                        />
+                                    </div>
+
+                                    :<div className="rounded-circle overflow-hidden" 
+                                        style={{width:"50px",height:"50px", backgroundColor:"grey"}}>
+                                        <FontAwesomeIcon icon={faUser} className="ic-light-grey rounded-circle w-100 h-100 pt-2"/>
+                                    </div>
+                                }
+                            </div>
+                            <div className=" d-flex flex-column gap-1">
+                                <span style={{fontWeight:"500"}}>{comment.userName.toUpperCase()}</span>
+                
+                                <span className="" style={{color:"grey",fontSize:"13px"}}>{formatDateTime(comment.dateAdded)}</span>
+                
+                                <p className="mt-3" style={{fontWeight:"300"}}>{comment.comment}</p>
+
+                                <div className=" d-flex gap-3 align-items-start">
+                                    <h6 className="fw-lighter">On: </h6>
+                                    <h6 className="" >{decodeString(comment.articleHeadline)}</h6>
+                                </div>
+                               
+                            </div>
+                            
+                        </div>
+                        })
+                    }
+                </div>
+            }
+       </div>
+               <SessionEndedModal showSessionEndedModal={showSessionEndedModal} setShowSessionEndedModal={setShowSessionEndedModal}/>
+
+
+    </div> 
+}
--- client/src/pages/resetPassword.jsx
+++ client/src/pages/resetPassword.jsx
@@ -1,175 +1,174 @@
 import React,{useState} from "react";
- import { useNavigate } from "react-router-dom";
- import { Modal } from "react-bootstrap";
- // import { useUserContext } from "../userContext";
- import api from "../config/api";
- import { useParams } from "react-router-dom";
- 
- 
- export default function ResetPassword(){
-     const navigate=useNavigate();
-     const [email,set_email]=useState();
-     const [newPassword,setNewPassword]=useState();
-     const [passwordConfirm,setPasswordConfirm]=useState();
-     const [errorMessage,setErrorMessage]=useState();
-     const {resetToken} = useParams();
-     const [isValidating, setIsValidating] = useState(false);
-     const [showSuccessModal, setShowSuccesModal] = useState(false);
-     const [successMessage, setSuccessMessage] = useState();
-     // const {user,contextLogin} = useUserContext();
- 
- 
-     function requestPasswordReset(e){
-         e.preventDefault();
-         setErrorMessage();
-         setIsValidating(true);
- 
-         api.post('/requestPasswordReset',
-             {
-                 withCredentials:true,
-                 email:email,
-             })
-             .then((response)=>{
-                 setIsValidating(false);
-                 if(response && response.status===200){
-                     setSuccessMessage(`A password reset link has been sent to your email. Please check your spam folder if you don't see it in your inbox.`);
-                     setShowSuccesModal(true);
-                 }
-             })
-             .catch((err)=>{
-                 setIsValidating(false);
-                 // document.write(err)
-                 if(err.response && err.response.status===401){
-                     setErrorMessage("Entered email is not registered")
-                 }
-             });
-     }
- 
- 
-     function validationErrors(){
-         var errorBuffer='';
- 
-         if(newPassword.length<4){
-             errorBuffer+= "*Password must have at least 4 characters";
- 
+import { useNavigate } from "react-router-dom";
+import { Modal } from "react-bootstrap";
+// import { useUserContext } from "../userContext";
+import api from "../config/api";
+import { useParams } from "react-router-dom";
+
+
+export default function ResetPassword(){
+    const navigate=useNavigate();
+    const [email,set_email]=useState();
+    const [newPassword,setNewPassword]=useState();
+    const [passwordConfirm,setPasswordConfirm]=useState();
+    const [errorMessage,setErrorMessage]=useState();
+    const {resetToken} = useParams();
+    const [isValidating, setIsValidating] = useState(false);
+    const [showSuccessModal, setShowSuccesModal] = useState(false);
+    const [successMessage, setSuccessMessage] = useState();
+    // const {user,contextLogin} = useUserContext();
+
+
+    function requestPasswordReset(e){
+        e.preventDefault();
+        setErrorMessage();
+        setIsValidating(true);
+
+        api.post('/requestPasswordReset',
+            {
+                withCredentials:true,
+                email:email,
+            })
+            .then((response)=>{
+                setIsValidating(false);
+                if(response && response.status===200){
+                    setSuccessMessage(`A password reset link has been sent to your email. Please check your spam folder if you don't see it in your inbox.`);
+                    setShowSuccesModal(true);
+                }
+            })
+            .catch((err)=>{
+                setIsValidating(false);
+                // document.write(err)
+                if(err.response && err.response.status===401){
+                    setErrorMessage("Entered email is not registered")
+                }
+            });
+    }
+
+
+    function validationErrors(){
+        var errorBuffer='';
+
+        if(newPassword.length<4){
+            errorBuffer+= "*Password must have at least 4 characters";
+
+        }
+        if(newPassword!==passwordConfirm){
+            errorBuffer+= "*Passwords don't match ";
          }
-         if(newPassword!==passwordConfirm){
-             errorBuffer+= "*Passwords don't match ";
-          }
- 
-         const emailRegex=/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-         if(!emailRegex.test(email)){
-             errorBuffer+= " *Invalid email"
-         }
-        
-         setErrorMessage(errorBuffer);
-         return errorBuffer;
-     }
- 
-     const resetPassword=(e)=>{
-         e.preventDefault();
-         if(validationErrors().length===0){
-             setIsValidating(true);
- 
-                 api.post("/resetPassword",
-                     {
-                         email:email,
-                         password:newPassword,
-                         resetToken:resetToken,
-                         withCredentials:true
-                     }
-                     ).then((response)=>{
-                             setIsValidating(false);                            
-                             if(response && response.status===201){
-                                 setSuccessMessage('Password reset successful!');
-                                 setShowSuccesModal(true);
-                             }
-                     })
-                 .catch((err)=>{
-                     setIsValidating(false);
-                 // document.write("the error:   ",err);
- 
-                     if(err.response.status===401){
-                         // document.write(err)
-                         setErrorMessage(err.response.data.errors);                    
-                     }
-                 });
-         }
-        
-     }
- 
- 
-     return(
-         <div className="auth-page ">
- 
-             <div id="auth-form" className="d-flex flex-column">
-                 <div className="d-flex justify-content-center">
-                     <img class="" src={require("../logos/logo.png")} style={{width:"48%"}} alt=""/>
-                     
-                 </div>
-                 <form onSubmit={requestPasswordReset} >
-                   
-                         <input className="col-md-12" type="text" name="email" placeholder="email" required
-                                 value={email} onChange={(e)=>{set_email(e.target.value)}}
-                         />
- 
-                         
-                         <div className={`${resetToken === 'null' ? 'd-none': ''}`}>
-                             <input className="col-md-12" type="password" name="password" placeholder="new password" required
-                                 value={newPassword} onChange={(e)=>{setNewPassword(e.target.value)}}
-                             />
- 
-                             <input className="col-md-12" type="password" name="password" placeholder="confirm password" required
-                                 value={passwordConfirm} onChange={(e)=>{setPasswordConfirm(e.target.value)}}
-                             />
-                         </div>
-                         
-                     
-                     <p style={{color:"red"}}>{errorMessage}</p>
- 
-                     {
-                         resetToken === 'null'
-                         ?<button className="col-md-12 btn-submit" id="" type="submit" onClick={requestPasswordReset}>
+
+        const emailRegex=/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        if(!emailRegex.test(email)){
+            errorBuffer+= " *Invalid email"
+        }
+       
+        setErrorMessage(errorBuffer);
+        return errorBuffer;
+    }
+
+    const resetPassword=(e)=>{
+        e.preventDefault();
+        if(validationErrors().length===0){
+            setIsValidating(true);
+
+                api.post("/resetPassword",
+                    {
+                        email:email,
+                        password:newPassword,
+                        resetToken:resetToken,
+                        withCredentials:true
+                    }
+                    ).then((response)=>{
+                            setIsValidating(false);                            
+                            if(response && response.status===201){
+                                setSuccessMessage('Password reset successful!');
+                                setShowSuccesModal(true);
+                            }
+                    })
+                .catch((err)=>{
+                    setIsValidating(false);
+                // document.write("the error:   ",err);
+
+                    if(err.response.status===401){
+                        // document.write(err)
+                        setErrorMessage(err.response.data.errors);                    
+                    }
+                });
+        }
+       
+    }
+
+
+    return(
+        <div className="auth-page ">
+
+            <div id="auth-form" className="d-flex flex-column">
+                <div className="d-flex justify-content-center">
+                    <img class="" src={require("../logos/logo.png")} style={{width:"48%"}} alt=""/>
+                    
+                </div>
+                <form onSubmit={requestPasswordReset} >
+                  
+                        <input className="col-md-12" type="text" name="email" placeholder="email" required
+                                value={email} onChange={(e)=>{set_email(e.target.value)}}
+                        />
+
+                        
+                        <div className={`${resetToken === 'null' ? 'd-none': ''}`}>
+                            <input className="col-md-12" type="password" name="password" placeholder="new password" required
+                                value={newPassword} onChange={(e)=>{setNewPassword(e.target.value)}}
+                            />
+
+                            <input className="col-md-12" type="password" name="password" placeholder="confirm password" required
+                                value={passwordConfirm} onChange={(e)=>{setPasswordConfirm(e.target.value)}}
+                            />
+                        </div>
+                        
+                    
+                    <p style={{color:"red"}}>{errorMessage}</p>
+
+                    {
+                        resetToken === 'null'
+                        ?<button className="col-md-12 btn-submit" id="" type="submit" onClick={requestPasswordReset}>
+                            {isValidating
+                                ?<div className="spinner-border text-white">
+                                    <span className="sr-only">Validating...</span>
+                                </div>
+                                :<span>Submit</span>
+                            }
+                        </button>
+                        :<button className="col-md-12 btn-submit" id="" type="submit" onClick={resetPassword}>
                              {isValidating
-                                 ?<div className="spinner-border text-white">
-                                     <span className="sr-only">Validating...</span>
-                                 </div>
-                                 :<span>Submit</span>
-                             }
-                         </button>
-                         :<button className="col-md-12 btn-submit" id="" type="submit" onClick={resetPassword}>
-                              {isValidating
-                                 ?<div className="spinner-border text-white">
-                                     <span className="sr-only">Validating...</span>
-                                 </div>
-                                 :<span>Change Password</span>
-                             }
-                         </button>
-                     }
-                     
- 
-                     
-                 </form>
-             </div>
- 
-             <Modal show={showSuccessModal} centered>
-                     <Modal.Body>
-                         <div className=" d-flex flex-column justify-content-center">
-                             <p>{successMessage}</p>
-                             <div>
-                                 <button className=" col-12 btn btn-light" 
-                                     onClick={()=>{
-                                         setShowSuccesModal(false);
-                                         navigate("/login");
-                                     }}>
-                                     Okay
-                                 </button>
-                             </div>
-                         </div>
-                     </Modal.Body>
-             </Modal>
-         </div>
-         
-     )
- }
- 
+                                ?<div className="spinner-border text-white">
+                                    <span className="sr-only">Validating...</span>
+                                </div>
+                                :<span>Change Password</span>
+                            }
+                        </button>
+                    }
+                    
+
+                    
+                </form>
+            </div>
+
+            <Modal show={showSuccessModal} centered>
+                    <Modal.Body>
+                        <div className=" d-flex flex-column justify-content-center">
+                            <p>{successMessage}</p>
+                            <div>
+                                <button className=" col-12 btn btn-light" 
+                                    onClick={()=>{
+                                        setShowSuccesModal(false);
+                                        navigate("/login");
+                                    }}>
+                                    Okay
+                                </button>
+                            </div>
+                        </div>
+                    </Modal.Body>
+            </Modal>
+        </div>
+        
+    )
+}
