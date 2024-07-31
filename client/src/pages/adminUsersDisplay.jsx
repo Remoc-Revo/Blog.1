f7a021b1eafe367285ccfdfbda9a9fff4fbfcff9
Remoc-Revo <timothynguono@gmail.com>
2024-07-31 17:49:40 +0300

    remove node modules

--- client/src/pages/adminUsersDisplay.jsx
+++ client/src/pages/adminUsersDisplay.jsx
@@ -8,154 +8,154 @@ import moment from "moment";
 import SessionEndedModal from "../reusables/sessionEndedModal";
 
 export default function AdminUsersDisplay(){
-    const [allUsers, setAllUsers] = useState([]);
-    const [displayedUsers, setDisplayedUsers] = useState([])
-    const [searchedText, setSearchedText] = useState('');
-    const [isSearchInputActive, setIsSearchInputActive] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [showSessionEndedModal, setShowSessionEndedModal] = useState(false);
-    const navigate = useNavigate();
+   const [allUsers, setAllUsers] = useState([]);
+   const [displayedUsers, setDisplayedUsers] = useState([])
+   const [searchedText, setSearchedText] = useState('');
+   const [isSearchInputActive, setIsSearchInputActive] = useState(false);
+   const [isLoading, setIsLoading] = useState(false);
+   const [showSessionEndedModal, setShowSessionEndedModal] = useState(false);
+   const navigate = useNavigate();
 
-    useEffect(()=>{
-        setIsLoading(true);
-        api.get('/users',)
-            .then((response)=>{
-                console.log("fetched users",response.data.users);
-                setAllUsers(response.data.users)
-                setDisplayedUsers(response.data.users)
-                setIsLoading(false)
-            })
-            .catch((e)=>{
-                setIsLoading(false);
-                console.log("Error fetching users: ",e);
-                if(e.response.status === 401){
-                    navigate('/');
-                    setShowSessionEndedModal(true);
+   useEffect(()=>{
+       setIsLoading(true);
+       api.get('/users',)
+           .then((response)=>{
+               console.log("fetched users",response.data.users);
+               setAllUsers(response.data.users)
+               setDisplayedUsers(response.data.users)
+               setIsLoading(false)
+           })
+           .catch((e)=>{
+               setIsLoading(false);
+               console.log("Error fetching users: ",e);
+               if(e.response.status === 401){
+                   navigate('/');
+                   setShowSessionEndedModal(true);
+               }
+           })
+   },[])
+
+
+   function handleSearch(e){
+       const text = e.target.value.toLowerCase();
+       setSearchedText(text);     
+
+       const filteredUsers = allUsers.filter(
+           user => 
+               {
+                   const userName = user.userName.toLowerCase();
+                   const userEmail = user.userEmail.toLowerCase();
+                   
+                   if(userName.includes(text) || userEmail.includes(text)){            
+                       return user;
+                   }
+
+                   return null;
                 }
-            })
-    },[])
+       )
 
-
-    function handleSearch(e){
-        const text = e.target.value.toLowerCase();
-        setSearchedText(text);     
-
-        const filteredUsers = allUsers.filter(
-            user => 
-                {
-                    const userName = user.userName.toLowerCase();
-                    const userEmail = user.userEmail.toLowerCase();
-                    
-                    if(userName.includes(text) || userEmail.includes(text)){            
-                        return user;
-                    }
-
-                    return null;
-                 }
-        )
-
-        setDisplayedUsers(filteredUsers);
-    }
+       setDisplayedUsers(filteredUsers);
+   }
 
 
 
-    return <div className="container col-lg-7 col-md-9">
+   return <div className="container col-lg-7 col-md-9">
 
-        <h3 className="mb-5">All Users {allUsers.length !== 0 && `(${allUsers.length})`}</h3>
-        <div className="d-flex  mb-4 " id="users-search">
-            <div className="col-12 position-relative d-flex justify-content-between border" >                
-                <button className="btn rounded-0">
-                    <FontAwesomeIcon icon={faSearch}/>
-                </button>                
-                
-                <div className="">
-                    <button className="btn position-absolute start-0 top-0 h-100"
-                        // onClick={}
-                        style={{zIndex:"2",color:"grey"}}>
-                        <FontAwesomeIcon icon={faSearch}/>
-                    </button>
-                    <input 
-                        type="text"
-                        className="position-absolute w-100  ps-5 pe-5 start-0 top-0 h-100 border-0"
-                        placeholder=" Search user name or email..."
-                        onClick={()=>{setIsSearchInputActive(true)}}
-                        onChange={handleSearch}
-                        value={searchedText}
-                        style={{zIndex:"1"}}
-                        />
-                    
-                { isSearchInputActive 
-                    &&<button className="btn position-absolute end-0 top-0 h-100"
-                        onClick={()=>{
-                            setIsSearchInputActive(false);
-                            setSearchedText('')
-                            setDisplayedUsers(allUsers);
-                        }}
-                        style={{zIndex:"1"}}>
-                        <FontAwesomeIcon icon={faTimes} className="ic-grey"/>
-                    </button>}
-                </div>
-                    
-                
-            </div>
-                
-            </div>
+       <h3 className="mb-5">All Users {allUsers.length !== 0 && `(${allUsers.length})`}</h3>
+       <div className="d-flex  mb-4 " id="users-search">
+           <div className="col-12 position-relative d-flex justify-content-between border" >                
+               <button className="btn rounded-0">
+                   <FontAwesomeIcon icon={faSearch}/>
+               </button>                
+               
+               <div className="">
+                   <button className="btn position-absolute start-0 top-0 h-100"
+                       // onClick={}
+                       style={{zIndex:"2",color:"grey"}}>
+                       <FontAwesomeIcon icon={faSearch}/>
+                   </button>
+                   <input 
+                       type="text"
+                       className="position-absolute w-100  ps-5 pe-5 start-0 top-0 h-100 border-0"
+                       placeholder=" Search user name or email..."
+                       onClick={()=>{setIsSearchInputActive(true)}}
+                       onChange={handleSearch}
+                       value={searchedText}
+                       style={{zIndex:"1"}}
+                       />
+                   
+               { isSearchInputActive 
+                   &&<button className="btn position-absolute end-0 top-0 h-100"
+                       onClick={()=>{
+                           setIsSearchInputActive(false);
+                           setSearchedText('')
+                           setDisplayedUsers(allUsers);
+                       }}
+                       style={{zIndex:"1"}}>
+                       <FontAwesomeIcon icon={faTimes} className="ic-grey"/>
+                   </button>}
+               </div>
+                   
+               
+           </div>
+               
+           </div>
 
-            <div>
-            {
-                isLoading &&
-                <div className="d-flex justify-content-center">
-                    <div className="spinner-border text-success ">
-                        <span className="sr-only">Loading...</span>
-                    </div>
-                </div>
-            }
-            <table className="w-100 " >
-                {
-                    displayedUsers.map(
-                        user=><tr className="" >
-                            <div className=" d-flex gap-4 ms-4 me-4 pt-2 align-items-start" style={{height:"100px"}}>
-                            {
-                                    (user.photoUrl !== null)
-                                    ?<div style={{width:"70px",height:"70px"}}>                                                                                           
-                                        <img src={user.photoUrl}
-                                            className="rounded-circle w-100 h-100 object-fit-cover"
-                                            alt=""
-                                        />
-                                    </div>
+           <div>
+           {
+               isLoading &&
+               <div className="d-flex justify-content-center">
+                   <div className="spinner-border text-success ">
+                       <span className="sr-only">Loading...</span>
+                   </div>
+               </div>
+           }
+           <table className="w-100 " >
+               {
+                   displayedUsers.map(
+                       user=><tr className="" >
+                           <div className=" d-flex gap-4 ms-4 me-4 pt-2 align-items-start" style={{height:"100px"}}>
+                           {
+                                   (user.photoUrl !== null)
+                                   ?<div style={{width:"70px",height:"70px"}}>                                                                                           
+                                       <img src={user.photoUrl}
+                                           className="rounded-circle w-100 h-100 object-fit-cover"
+                                           alt=""
+                                       />
+                                   </div>
 
-                                    :<div className="rounded-circle overflow-hidden" 
-                                        style={{width:"70px",height:"70px", backgroundColor:"lightgrey"}}>
-                                        <FontAwesomeIcon icon={faUser} className="ic-white rounded-circle w-100 h-100 pt-2"/>
-                                    </div>
-                                }
-                                <div>
-                                    <span className="" style={{fontWeight:"500"}}>{user.userName}</span>
-                                    <span className="d-block" style={{fontWeight:"300"}}>{user.userEmail}</span>
+                                   :<div className="rounded-circle overflow-hidden" 
+                                       style={{width:"70px",height:"70px", backgroundColor:"lightgrey"}}>
+                                       <FontAwesomeIcon icon={faUser} className="ic-white rounded-circle w-100 h-100 pt-2"/>
+                                   </div>
+                               }
+                               <div>
+                                   <span className="" style={{fontWeight:"500"}}>{user.userName}</span>
+                                   <span className="d-block" style={{fontWeight:"300"}}>{user.userEmail}</span>
 
-                                    {
-                                        user.userLevel === 0 &&
-                                        (<div className="mt-1">
-                                            <span style={{color:"teal"}}>Registered</span>
-                                            <span className="fw-light"> {moment(user.userRegistrationDate).fromNow()}</span>
-                                        </div>)                                        
-                                    }
-                                    {     user.userLevel === 1 &&
-                                        (<div>
-                                            <span style={{color:"orange"}}>Admin</span>
-                                        </div>)
-                                    }
-                                </div>
-                            </div>
-                            
-                                
-                        </tr>
-                    )
-                }
-            </table>
-        </div>
-                <SessionEndedModal showSessionEndedModal={showSessionEndedModal} setShowSessionEndedModal={setShowSessionEndedModal}/>
+                                   {
+                                       user.userLevel === 0 &&
+                                       (<div className="mt-1">
+                                           <span style={{color:"teal"}}>Registered</span>
+                                           <span className="fw-light"> {moment(user.userRegistrationDate).fromNow()}</span>
+                                       </div>)                                        
+                                   }
+                                   {     user.userLevel === 1 &&
+                                       (<div>
+                                           <span style={{color:"orange"}}>Admin</span>
+                                       </div>)
+                                   }
+                               </div>
+                           </div>
+                           
+                               
+                       </tr>
+                   )
+               }
+           </table>
+       </div>
+               <SessionEndedModal showSessionEndedModal={showSessionEndedModal} setShowSessionEndedModal={setShowSessionEndedModal}/>
 
 
-    </div>
+   </div>
 }
