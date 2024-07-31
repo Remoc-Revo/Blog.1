c21a1a0c0be405c2b56c404a1579e375385cc1e1
Remoc-Revo <timothynguono@gmail.com>
2024-07-31 12:16:41 +0300

    Seperate client server from main client codnt code

---
+++ client_server/index.js
@@ -0,0 +1,10 @@
+console.log("Ignoreee importation ")
+ 
+require('ignore-styles');
+
+require("@babel/register")({
+  ignore: [/(node_modules)/],
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+});
+
+require("./server");
